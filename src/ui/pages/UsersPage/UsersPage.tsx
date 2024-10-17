7f7e53389189052ce1a19f12f019a7f1572522f9
Ricardo Solorzano <ricardo@ese.plus>
2024-10-17 14:39:57 -0600

    agregando estructura para page de usuarios

--- src/ui/pages/UsersPage/UsersPage.tsx
+++ src/ui/pages/UsersPage/UsersPage.tsx
@@ -1,3 +1,69 @@
+import { Button, Table } from "antd";
+import Column from "antd/es/table/Column";
+import { ModalUI } from "../../components/ModalUI/ModalUI";
+
 export const UsersPage = () => {
-  return <div>User</div>;
+  const data = [
+    {
+      id: "1",
+      firstName: "John",
+      lastName: "Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      id: "2",
+      firstName: "Jim",
+      lastName: "Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      id: "3",
+      firstName: "Joe",
+      lastName: "Black",
+      age: 32,
+      address: "Sydney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
+
+  return (
+    <div>
+      <Button type="primary">Agregar Usuario</Button>
+      <br />
+      <br />
+      <Table
+        rowKey={(record) => record.id}
+        dataSource={data}
+        pagination={false}
+      >
+        <Column title="Nombre" dataIndex="firstName" key="firstName" />
+        <Column title="Apellido" dataIndex="lastName" key="lastName" />
+        <Column title="Edad" dataIndex="age" key="age" />
+        <Column title="Direccion" dataIndex="address" key="address" />
+        <Column
+          title="Action"
+          key="action"
+          render={(_, record) => (
+            <div style={{ display: "flex", gap: 8 }}>
+              <Button variant="solid" onClick={() => console.log("A", record)}>
+                Editar
+              </Button>
+              <Button
+                variant="solid"
+                color="danger"
+                onClick={() => console.log("B", record)}
+              >
+                Eliminar
+              </Button>
+            </div>
+          )}
+        />
+      </Table>
+      <ModalUI />
+    </div>
+  );
 };
