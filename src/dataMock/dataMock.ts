d99f805567a5933f3a39eba98ce73deb6fdf2844
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-10-18 14:37:37 -0600

    agregando una propiedad para el mock

--- src/dataMock/dataMock.ts
+++ src/dataMock/dataMock.ts
@@ -27,40 +27,48 @@ export const dataTypeUsers = [
     id: "1",
     name: "Admin",
     description: "Admin description",
+    color: "red",
   },
   {
     id: "2",
     name: "User",
     description: "User description",
+    color: "blue",
   },
   {
     id: "3",
     name: "Guest",
     description: "Guest description",
+    color: "green",
   },
   {
     id: "4",
     name: "Moderator",
     description: "Moderator description",
+    color: "orange",
   },
   {
     id: "5",
     name: "SuperAdmin",
     description: "SuperAdmin description",
+    color: "purple",
   },
   {
     id: "6",
     name: "SuperUser",
     description: "SuperUser description",
+    color: "pink",
   },
   {
     id: "7",
     name: "SuperGuest",
     description: "SuperGuest description",
+    color: "yellow",
   },
   {
     id: "8",
     name: "SuperModerator",
     description: "SuperModerator description",
+    color: "cyan",
   },
 ];
