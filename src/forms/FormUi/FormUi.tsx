b3c0d84bd9299809afe1fe5f3d44024f6012f1b2
Ricardo Solorzano <ricardo@ese.plus>
2024-10-21 09:15:02 -0600

    agregando formulario base

---
+++ src/forms/FormUi/FormUi.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { Form, FormProps } from "antd";
+
+type Props<T> = Omit<FormProps<T>, "children" > & { children: ReactNode };
+
+
+export const FormUi = <T = never>(props: Props<T>) => {
+    return (
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            style={{
+                maxWidth: 600,
+            }}
+            autoComplete="off"
+            {...props}
+        />
+    );
+};
