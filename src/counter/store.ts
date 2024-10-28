e4942922abf759b57df571038b2f971e3d54dbc1
Ricardo Solorzano <ricardo@ese.plus>
2024-10-28 13:11:12 -0600

    servicio uso e implementacion

--- src/counter/store.ts
+++ src/counter/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
+import { pokemonApi } from "./services/pokemon";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        [pokemonApi.reducerPath]: pokemonApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(pokemonApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
