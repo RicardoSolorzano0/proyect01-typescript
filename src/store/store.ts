54ee0dadc54098154125ed1e4b9da9a18b78f25f
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-10-31 09:06:53 -0600

    arreglando el store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "@/store/slices/counterSlice";
-import { pokemonApi } from "@/services/pokemon";
+import { pokemonApi } from "@/api/services/pokemon";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { userTypesApi } from "@/services/userTypes";
-import { userApi } from "@/services/user";
+import { userAppApi } from "@/api/rtk/userApp.api";
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         [pokemonApi.reducerPath]: pokemonApi.reducer,
-        [userTypesApi.reducerPath]: userTypesApi.reducer,
-        [userApi.reducerPath]: userApi.reducer
+        // [userTypesApi.reducerPath]: userTypesApi.reducer,
+        // [userApi.reducerPath]: userApi.reducer
+        [userAppApi.reducerPath]: userAppApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
         .concat(pokemonApi.middleware)
-        .concat(userTypesApi.middleware)
-        .concat(userApi.middleware),
+        // .concat(userTypesApi.middleware)
+        // .concat(userApi.middleware),
+        .concat(userAppApi.middleware)
 })
 
 setupListeners(store.dispatch)
