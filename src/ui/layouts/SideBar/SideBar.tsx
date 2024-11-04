19f2da0914534e86196eb4fbcb936b7b5b11cc7f
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-04 14:41:40 -0600

    agregando componente para poder cambiar de idioma

--- src/ui/layouts/SideBar/SideBar.tsx
+++ src/ui/layouts/SideBar/SideBar.tsx
@@ -2,6 +2,7 @@ import { Menu } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { menu } from "@/router/menu";
 import { Link } from "react-router-dom";
+import { LanguageSelector } from "@/ui/components/LanguageSelector";
 
 const items2 = menu.map((item) => ({
   key: item.path,
@@ -12,14 +13,19 @@ const items2 = menu.map((item) => ({
 export const SideBar = () => {
   return (
     <Sider className="overflow-auto h-screen fixed inset-y-0">
-      <div className="demo-logo-vertical" />
-      <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={["1"]}
-        items={items2}
-        onChange={() => {}}
-      />
+      <div className="h-full">
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={items2}
+          onChange={() => { }}
+        />
+
+        <div className="absolute bottom-0 right-0 p-4">
+          <LanguageSelector />
+        </div>
+      </div>
     </Sider>
   );
 };
