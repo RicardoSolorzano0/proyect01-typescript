6a33f1e679dd3da19c22f50cfd8fa1fb0aefa828
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-04 14:44:07 -0600

    colocamos los locales de los que nos provee antd

---
+++ src/i18n/antdLocales.ts
@@ -0,0 +1,8 @@
+import type { Locale } from 'antd/es/locale';
+import enUS            from 'antd/es/locale/en_US';
+import esES            from 'antd/es/locale/es_ES';
+
+export const antdLocales: Record<string, Locale> = Object.freeze({
+    en: enUS,
+    es: esES
+});
