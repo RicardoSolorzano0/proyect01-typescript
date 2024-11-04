d872f32280ffafe5f16b04eda1a3fcca5118a7b9
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-04 14:35:00 -0600

    componente para los iconos

---
+++ src/ui/components/FeatherIcon.tsx
@@ -0,0 +1,10 @@
+import Icon from '@ant-design/icons';
+import type { ComponentProps, ComponentType, SVGProps } from 'react';
+
+
+type Props = {
+    readonly icon: ComponentType<SVGProps<SVGSVGElement>>;
+} & ComponentProps<typeof Icon>;
+
+export const FeatherIcon = ({ icon, ...props }: Props) =>
+    <Icon component={icon} {...props} className={`${props.className} feather-icon`} />;
