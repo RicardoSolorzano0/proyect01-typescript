6633e1840449f0a2770f9ac10a5cbc984db0699d
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-04 14:52:04 -0600

    agregando la configuracion para que tome las fechas segun i18n y que los componentes de antd tambien lo tengan

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "@/router/router";
-import { App as ContexModal } from "antd";
+import { ConfigProvider, App as ContexModal } from "antd";
 import { Provider } from "react-redux";
 import { store } from '@/store';
+import { useTranslation } from "react-i18next";
+import { antdLocales } from "./i18n/antdLocales";
+import { locale } from "dayjs";
+import 'dayjs/locale/es';
 
 const App = () => {
+  const { i18n } = useTranslation();
+  locale(i18n.language);
+
   return (
     <Provider store={store}>
-    <ContexModal>
-      <RouterProvider router={router} />
-    </ContexModal>
+      <ConfigProvider locale={antdLocales[i18n.language]}>
+        <ContexModal>
+          <RouterProvider router={router} />
+        </ContexModal>
+      </ConfigProvider>
     </Provider>
   );
 };
