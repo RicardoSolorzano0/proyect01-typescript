948a6d4e57b0206716133af36ca9dd661050981a
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-06 10:34:21 -0600

    agregando las columnas que hacen falta

--- src/ui/pages/UsersPage/table/columns.ts
+++ src/ui/pages/UsersPage/table/columns.ts
@@ -1,6 +1,7 @@
 import { User } from '@/types/User';
 import type { TableColumnsType } from 'antd';
-import type { TFunction }        from 'i18next';
+import type { TFunction } from 'i18next';
+import dayjs from 'dayjs';
 
 
 export const columns = (t: TFunction<'users'>): TableColumnsType<User> => [
@@ -11,5 +12,18 @@ export const columns = (t: TFunction<'users'>): TableColumnsType<User> => [
     {
         dataIndex: 'last_name',
         title: t('table.lastName')
+    },
+    {
+        dataIndex: 'email',
+        title: t('table.email')
+    },
+    {
+        dataIndex: 'birthdate',
+        title: t('table.birthdate'),
+        render: (value) => dayjs(value).format('L')
+    },
+    {
+        dataIndex: 'address',
+        title: t('table.address')
     }
 ];
