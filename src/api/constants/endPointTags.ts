82b3889787149155ce79dd4921a9adbfe03b8216
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-07 05:28:56 -0600

    agregando los tags y uri para el modulo de animals

--- src/api/constants/endPointTags.ts
+++ src/api/constants/endPointTags.ts
@@ -2,6 +2,7 @@ export const LIST_ID = 'LIST';
 
 export const USERS_TAG = 'USERS';
 export const USER_TYPES_TAG = 'USER_TYPES';
+export const ANIMALS_TAG = 'ANIMALS';
 
 export const ENDPOINT_TAGS = [
     USERS_TAG,
---
+++ src/api/constants/uris/animal.uri.ts
@@ -0,0 +1,7 @@
+export const animalUris = Object.freeze({
+    deleteAnimal: '/deleteAnimal',
+    createAnimal: '/createAnimal',
+    updateAnimal: '/updateAnimal',
+    selectAnimals: '/selectAnimals',
+    selectPaginatedAnimals: '/selectPaginatedAnimals'
+})
