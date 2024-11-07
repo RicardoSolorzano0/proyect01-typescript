6ffcf1c4ee13ce95d1ea5df19f814eb1702c0cad
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-07 05:27:24 -0600

    agregando traduccion para modulo de animales

--- src/i18n/locales/en/index.ts
+++ src/i18n/locales/en/index.ts
@@ -1,9 +1,11 @@
 import common from './common.json';
 import users from './users.json';
 import usersTypes from './usersTypes.json';
+import animals from './animals.json';
 
 export const enLocales = {
     common,
     users,
-    usersTypes
+    usersTypes,
+    animals
 }
--- src/i18n/locales/es/index.ts
+++ src/i18n/locales/es/index.ts
@@ -1,9 +1,11 @@
 import common from './common.json';
 import users from './users.json';
 import usersTypes from './usersTypes.json';
+import animals from "./animals.json"
 
 export const esLocales = {
     common,
     users,
-    usersTypes
+    usersTypes,
+    animals
 }
