6bb33d1dc0de414feabdcb688b446903118c38a7
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 11:20:48 -0600

    agregando linter a icons y layout

--- src/ui/layouts/ContainerLayout/ContainerLayout.tsx
+++ src/ui/layouts/ContainerLayout/ContainerLayout.tsx
@@ -1,67 +1,70 @@
-import { Layout } from "antd";
-import { SideBar } from "@/ui/layouts/SideBar/SideBar";
-import { Outlet, useNavigate } from "react-router";
-import { useEffect, useState } from "react";
-import { useFirebaseContext } from "@/context/FirebaseCtx";
-import { onAuthStateChanged } from "firebase/auth";
-import { routes } from "@/constants/routes";
-import { useAppDispatch } from "@/hooks";
-import { logoutUser, setCurrentUser } from "@/store/slices/userSlice";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Layout } from 'antd';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router';
+import { routes } from '@/constants/routes';
+import { useFirebaseContext } from '@/context/FirebaseContext';
+import { useAppDispatch } from '@/hooks';
+import { logoutUser, setCurrentUser } from '@/store/slices/userSlice';
+import { SideBar } from '@/ui/layouts/SideBar/SideBar';
 
 const { Content, Footer } = Layout;
 
 export const ContainerLayout = () => {
-  const navigate = useNavigate();
-  const { auth } = useFirebaseContext();
-  const dispatch = useAppDispatch()
-  const [loading, setLoading] = useState(true)
+    const navigate = useNavigate();
+    const { auth } = useFirebaseContext();
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setLoading(true)
-      if (!user) {
-        dispatch(logoutUser())
-        navigate(routes.login);
-        setLoading(false)
-      }else{
-        dispatch(setCurrentUser({
-          displayName: user.displayName?user.displayName:"User",
-          email: user.email!,
-          uid: user.uid
-        }))
-        setLoading(false)
-      } 
-  });
-  }, []);
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setLoading(true);
+            if (!user) {
+                dispatch(logoutUser());
+                navigate(routes.login);
+                setLoading(false);
+            } else {
+                dispatch(setCurrentUser({
+                    displayName: user.displayName ? user.displayName : 'User',
+                    email: user.email!,
+                    uid: user.uid
+                }));
+                setLoading(false);
+            } 
+        });
+    }, []);
 
-  if(loading){
-    return <>
-        <div className="flex items-center justify-center bg-[#001529] h-screen">
-            <div className="bg-white rounded-lg p-5 w-[400px]">
-                <div className="flex justify-center gap-2">
+    if (loading) {
+        return (
+            <div className='flex h-screen items-center justify-center bg-[#001529]'>
+                <div className='w-[400px] rounded-lg bg-white p-5'>
+                    <div className='flex justify-center gap-2'>
                     Cargando
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-}
+        );
+    }
 
-  return (
-    <Layout hasSider>
-      <SideBar />
-      <Layout className="ml-[200px]">
-        <Content
-          className="m-5 overflow-auto"
-          style={{ height: "calc(100vh - 109px)" }}
-        >
-          <div className="p-6 bg-white rounded-lg">
-            <Outlet />
-          </div>
-        </Content>
-        <Footer className="text-center">
+    return (
+        <Layout hasSider>
+            <SideBar />
+
+            <Layout className='ml-[200px]'>
+                <Content
+                    className='m-5 overflow-auto'
+                    style={ { height: 'calc(100vh - 109px)' } }
+                >
+                    <div className='rounded-lg bg-white p-6'>
+                        <Outlet />
+                    </div>
+                </Content>
+
+                <Footer className='text-center'>
           App Users ©{new Date().getFullYear()} Creado por Ricardo Solórzano
-        </Footer>
-      </Layout>
-    </Layout>
-  );
+                </Footer>
+            </Layout>
+        </Layout>
+    );
 };
--- src/ui/layouts/SideBar/SideBar.tsx
+++ src/ui/layouts/SideBar/SideBar.tsx
@@ -1,51 +1,70 @@
-import { Switch } from 'antd';
-import { Button, Menu, Tooltip } from "antd";
-import Sider from "antd/es/layout/Sider";
-import { menu } from "@/router/menu";
-import { Link } from "react-router-dom";
-import { LanguageSelector } from "@/ui/components/LanguageSelector";
-import { LogOut } from "react-feather";
-import { logoutFirebase } from "@/firebase/providers";
-import { useAppDispatch } from "@/hooks";
-import { logoutUser } from "@/store/slices/userSlice";
-import { useState } from "react";
-import { MenuTheme } from "antd/lib";
+import { Button, Menu, Switch, Tooltip } from 'antd';
 
-const items2 = menu.map((item) => ({
-  key: item.path,
-  icon: item.icon,
-  label: <Link to={item.path}>{item.label}</Link>,
+import Sider from 'antd/es/layout/Sider';
+import type { MenuTheme } from 'antd/lib';
+import { useState } from 'react';
+import { LogOut } from 'react-feather';
+import { Link } from 'react-router-dom';
+import { logoutFirebase } from '@/firebase/providers';
+import { useAppDispatch } from '@/hooks';
+import { menu } from '@/router/menu';
+import { logoutUser } from '@/store/slices/userSlice';
+import { LanguageSelector } from '@/ui/components/LanguageSelector';
+
+const items2 = menu.map(item => ({
+    icon: item.icon,
+    key: item.path,
+    label: <Link to={ item.path }>{item.label}</Link>
 }));
 
 export const SideBar = () => {
-  const dispatch = useAppDispatch()
-  const [theme, setTheme] = useState<MenuTheme>("dark");
+    const dispatch = useAppDispatch();
+    const [theme, setTheme] = useState<MenuTheme>('dark');
 
-  return (
-    <Sider theme={theme} className="overflow-auto h-screen fixed inset-y-0">
-      <div className="h-full">
-        <Menu
-          theme={theme}
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={items2}
-          onChange={() => { }}
-        />
-        <div className="ml-6 mt-4">
-          <Switch defaultChecked onChange={() => setTheme(theme === "light" ? "dark" : "light")} />
-        </div>
-        <div className="absolute bottom-0 right-0 p-4">
-          <Tooltip placement="left" title="Logout">
-            <Button shape="circle" icon={<LogOut />} onClick={async () => {
-              await logoutFirebase()
-              dispatch(logoutUser())
-            }} />
-          </Tooltip>
-        </div>
-        <div className="absolute bottom-0 left-0 p-4">
-          <LanguageSelector />
-        </div>
-      </div>
-    </Sider>
-  );
+
+    const handleLogout = async () =>{
+        await logoutFirebase();
+        dispatch(logoutUser());
+    };
+
+    return (
+        <Sider
+            className='fixed inset-y-0 h-screen overflow-auto'
+            theme={ theme }
+        >
+            <div className='h-full'>
+                <Menu
+                    defaultSelectedKeys={ ['1'] }
+                    items={ items2 }
+                    mode='inline'
+                    theme={ theme }
+                    onChange={ () => { } }
+                />
+
+                <div className='ml-6 mt-4'>
+                    <Switch
+                        defaultChecked
+                        onChange={ () => setTheme(theme === 'light' ? 'dark' : 'light') }
+                    />
+                </div>
+
+                <div className='absolute bottom-0 right-0 p-4'>
+                    <Tooltip
+                        placement='left'
+                        title='Logout'
+                    >
+                        <Button
+                            icon={ <LogOut /> }
+                            shape='circle'
+                            onClick={ ()=> void handleLogout() }
+                        />
+                    </Tooltip>
+                </div>
+
+                <div className='absolute bottom-0 left-0 p-4'>
+                    <LanguageSelector />
+                </div>
+            </div>
+        </Sider>
+    );
 };
