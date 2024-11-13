a034ae7371936a0aad2c2aa145c75d218614b3e8
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 09:25:22 -0600

    cambio de nombre en variable de entorno

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,24 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { 
+    // connectAuthEmulator,
+    getAuth } from 'firebase/auth';
 
 const {
     VITE_API_KEY,
-    VITE_AUTH_DOMAIN,
-    VITE_PROJECT_ID,
-    VITE_STORAGE_BUCKET,
-    VITE_MESSAGING_SENDERID,
     VITE_APP_ID,
-    VITE_MEASUREMENT_ID
-} = import.meta.env
+    VITE_AUTH_DOMAIN,
+    VITE_MEASUREMENT_ID,
+    VITE_MESSAGING_SENDER_ID,
+    VITE_PROJECT_ID,
+    VITE_STORAGE_BUCKET
+} = import.meta.env;
 
 const firebaseConfig = {
     apiKey: VITE_API_KEY,
-    authDomain: VITE_AUTH_DOMAIN,
-    projectId: VITE_PROJECT_ID,
-    storageBucket: VITE_STORAGE_BUCKET,
-    messagingSenderId: VITE_MESSAGING_SENDERID,
     appId: VITE_APP_ID,
-    measurementId: VITE_MEASUREMENT_ID
+    authDomain: VITE_AUTH_DOMAIN,
+    measurementId: VITE_MEASUREMENT_ID,
+    messagingSenderId: VITE_MESSAGING_SENDER_ID,
+    projectId: VITE_PROJECT_ID,
+    storageBucket: VITE_STORAGE_BUCKET
 };
 
 // Initialize Firebase
@@ -26,4 +28,4 @@ export const FirebaseApp = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 export const FirebaseAuth = getAuth(FirebaseApp);
- connectAuthEmulator(FirebaseAuth, "http://127.0.0.1:9099");
+// connectAuthEmulator(FirebaseAuth, 'http://127.0.0.1:9099');
