c6d3e5bd38c3211d01f9baf7d3134441a9fc1fc1
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 05:32:31 -0600

    agregando posibilida de cambiar el tema

--- src/ui/layouts/SideBar/SideBar.tsx
+++ src/ui/layouts/SideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+import { Switch } from 'antd';
 import { Button, Menu, Tooltip } from "antd";
 import Sider from "antd/es/layout/Sider";
 import { menu } from "@/router/menu";
@@ -7,6 +8,8 @@ import { LogOut } from "react-feather";
 import { logoutFirebase } from "@/firebase/providers";
 import { useAppDispatch } from "@/hooks";
 import { logoutUser } from "@/store/slices/userSlice";
+import { useState } from "react";
+import { MenuTheme } from "antd/lib";
 
 const items2 = menu.map((item) => ({
   key: item.path,
@@ -16,18 +19,21 @@ const items2 = menu.map((item) => ({
 
 export const SideBar = () => {
   const dispatch = useAppDispatch()
+  const [theme, setTheme] = useState<MenuTheme>("dark");
 
   return (
-    <Sider className="overflow-auto h-screen fixed inset-y-0">
+    <Sider theme={theme} className="overflow-auto h-screen fixed inset-y-0">
       <div className="h-full">
         <Menu
-          theme="dark"
+          theme={theme}
           mode="inline"
           defaultSelectedKeys={["1"]}
           items={items2}
           onChange={() => { }}
         />
-
+        <div className="ml-6 mt-4">
+          <Switch defaultChecked onChange={() => setTheme(theme === "light" ? "dark" : "light")} />
+        </div>
         <div className="absolute bottom-0 right-0 p-4">
           <Tooltip placement="left" title="Logout">
             <Button shape="circle" icon={<LogOut />} onClick={async () => {
