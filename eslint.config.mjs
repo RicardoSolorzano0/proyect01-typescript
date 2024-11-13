2fbccc23047899b5f7b3ccd64cb9cd8f25ee159f
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 09:26:35 -0600

    configuracion de linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import tailwind from "eslint-plugin-tailwindcss";
+import importPlugin from 'eslint-plugin-import';
+import cssPlugin from "eslint-plugin-css"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,210 +23,220 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default [...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:css/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:import/recommended",
-    //"plugin:perfectionist/recommended-natural",
-    "plugin:tailwindcss/recommended",
-)), {
-    plugins: {
-        "@stylistic": stylistic,
-        perfectionist: fixupPluginRules(perfectionist),
-        "react-refresh": reactRefresh,
-        unicorn,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+export default [
+    importPlugin.flatConfigs.recommended,
+    cssPlugin.configs["flat/standard"],
+    ...tailwind.configs["flat/recommended"],
+    ...fixupConfigRules(compat.extends(
+        "eslint:recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    )),
+    {
+        plugins: {
+            "@stylistic": stylistic,
+            perfectionist: fixupPluginRules(perfectionist),
+            "react-refresh": reactRefresh,
+            unicorn,
+            'jsx-a11y': jsxA11y,
+            react
         },
 
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "script",
-
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
-    },
-
-    settings: {
-        react: {
-            version: "detect",
-        },
-
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"],
-        },
-
-        "import/resolver": {
-            typescript: true,
-        },
-    },
-
-    rules: {
-        "@stylistic/block-spacing": "warn",
-        "@stylistic/brace-style": "warn",
-        "@stylistic/comma-dangle": "warn",
-        "@stylistic/comma-spacing": "warn",
-        "@stylistic/function-call-spacing": "warn",
-        "@stylistic/key-spacing": "warn",
-        "@stylistic/keyword-spacing": "warn",
-
-        "@stylistic/member-delimiter-style": ["warn", {
-            multiline: {
-                delimiter: "semi",
-                requireLast: true,
+        languageOptions: {
+            globals: {
+                ...globals.browser,
             },
 
-            singleline: {
-                delimiter: "semi",
-                requireLast: true,
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: "script",
+
+            parserOptions: {
+                projectService: "./tsconfig.json",
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+        },
+
+        settings: {
+            react: {
+                version: "detect",
             },
 
-            multilineDetection: "brackets",
-        }],
-
-        "@stylistic/object-curly-spacing": ["warn", "always"],
-
-        "@stylistic/quotes": ["error", "single", {
-            avoidEscape: true,
-            allowTemplateLiterals: true,
-        }],
-
-        "@stylistic/semi": "warn",
-        "@stylistic/space-before-blocks": "warn",
-        "@stylistic/space-infix-ops": "warn",
-        "@stylistic/type-annotation-spacing": "warn",
-        "@stylistic/jsx-closing-bracket-location": "warn",
-        "@stylistic/jsx-closing-tag-location": "warn",
-        "@stylistic/jsx-curly-brace-presence": "warn",
-        "@stylistic/jsx-curly-newline": "warn",
-        "@stylistic/jsx-curly-spacing": ["warn", "always"],
-        "@stylistic/jsx-equals-spacing": "warn",
-        "@stylistic/jsx-indent-props": "warn",
-        "@stylistic/jsx-quotes": ["warn", "prefer-single"],
-
-        "@stylistic/no-extra-parens": ["error", "all", {
-            conditionalAssign: true,
-            ignoreJSX: "multi-line",
-        }],
-
-        "@stylistic/arrow-parens": ["error", "as-needed"],
-        "@typescript-eslint/consistent-type-imports": "warn",
-        "@typescript-eslint/no-non-null-assertion": "off",
-
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-            caughtErrorsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/restrict-template-expressions": "off",
-
-        "import/order": ["error", {
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
+            "import/parsers": {
+                "@typescript-eslint/parser": [".ts", ".tsx"],
             },
 
-            groups: [
-                ["external", "builtin"],
-                "internal",
-                ["sibling", "parent"],
-                "index",
-                "unknown",
-            ],
+            "import/resolver": {
+                typescript: true,
+            },
+        },
 
-            pathGroups: [{
-                group: "internal",
-                pattern: "@/**/*.!(less)*(?*)",
-            }, {
-                group: "unknown",
-                pattern: "*.{less,css}*(?*)",
+        rules: {
+            "indent": ["warn", 4],
+            "import/no-unresolved": "off",
+            "@stylistic/block-spacing": "warn",
+            "@stylistic/brace-style": "warn",
+            "@stylistic/comma-dangle": "warn",
+            "@stylistic/comma-spacing": "warn",
+            "@stylistic/function-call-spacing": "warn",
+            "@stylistic/key-spacing": "warn",
+            "@stylistic/keyword-spacing": "warn",
 
-                patternOptions: {
-                    matchBase: true,
+            "@stylistic/member-delimiter-style": ["warn", {
+                multiline: {
+                    delimiter: "semi",
+                    requireLast: true,
                 },
 
-                position: "after",
+                singleline: {
+                    delimiter: "semi",
+                    requireLast: true,
+                },
+
+                multilineDetection: "brackets",
             }],
 
-            pathGroupsExcludedImportTypes: [],
-            warnOnUnassignedImports: true,
-        }],
+            "@stylistic/object-curly-spacing": ["warn", "always"],
 
-        "jsx-a11y/media-has-caption": "off",
-        "no-unused-vars": "off",
+            "@stylistic/quotes": ["error", "single", {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            }],
 
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+            "@stylistic/semi": "warn",
+            "@stylistic/space-before-blocks": "warn",
+            "@stylistic/space-infix-ops": "warn",
+            "@stylistic/type-annotation-spacing": "warn",
+            "@stylistic/jsx-closing-bracket-location": "warn",
+            "@stylistic/jsx-closing-tag-location": "warn",
+            "@stylistic/jsx-curly-brace-presence": "warn",
+            "@stylistic/jsx-curly-newline": "warn",
+            "@stylistic/jsx-curly-spacing": ["warn", "always"],
+            "@stylistic/jsx-equals-spacing": "warn",
+            "@stylistic/jsx-indent-props": "warn",
+            "@stylistic/jsx-quotes": ["warn", "prefer-single"],
 
-        "react/jsx-boolean-value": "error",
-        "react/jsx-newline": "error",
-        "react/jsx-no-useless-fragment": "error",
+            "@stylistic/no-extra-parens": ["error", "all", {
+                conditionalAssign: true,
+                ignoreJSX: "multi-line",
+            }],
 
-        "react/jsx-pascal-case": ["error", {
-            allowAllCaps: false,
-        }],
+            "@stylistic/arrow-parens": ["error", "as-needed"],
+            "@stylistic/jsx-first-prop-new-line": "warn",
+            "@stylistic/jsx-max-props-per-line": ["warn", { "maximum": 1 }],
 
-        "react/jsx-sort-props": ["warn", {
-            callbacksLast: true,
-            multiline: "last",
-            shorthandFirst: true,
-            reservedFirst: true,
-        }],
+            "@typescript-eslint/consistent-type-imports": "warn",
+            "@typescript-eslint/no-non-null-assertion": "off",
 
-        "react/jsx-wrap-multilines": ["warn", {
-            arrow: "parens-new-line",
-            assignment: "parens-new-line",
-            condition: "parens-new-line",
-            declaration: "parens-new-line",
-            logical: "parens-new-line",
-            prop: "parens-new-line",
-            return: "parens-new-line",
-        }],
+            "@typescript-eslint/no-unused-vars": ["error", {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_",
+            }],
 
-        "react/prefer-read-only-props": "warn",
-        "react/prop-types": "off",
-        "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/restrict-template-expressions": "off",
 
-        "react/self-closing-comp": ["error", {
-            component: true,
-            html: true,
-        }],
+            "import/order": ["error", {
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
 
-        "sort-imports": ["error", {
-            ignoreCase: true,
-            ignoreDeclarationSort: true,
-        }],
+                groups: [
+                    ["external", "builtin"],
+                    "internal",
+                    ["sibling", "parent"],
+                    "index",
+                    "unknown",
+                ],
 
-        "perfectionist/sort-imports": "off",
+                pathGroups: [{
+                    group: "internal",
+                    pattern: "@/**/*.!(less)*(?*)",
+                }, {
+                    group: "unknown",
+                    pattern: "*.{less,css}*(?*)",
 
-        // "perfectionist/sort-interfaces": ["warn", {
-        //     type: "natural",
-        //     "partition-by-new-line": true,
-        // }],
+                    patternOptions: {
+                        matchBase: true,
+                    },
 
-        "perfectionist/sort-jsx-props": "off",
-        "perfectionist/sort-named-imports": "off",
+                    position: "after",
+                }],
 
-        // "perfectionist/sort-objects": ["warn", {
-        //     type: "natural",
-        //     "ignore-pattern": ["tableActions"],
-        //     "partition-by-new-line": true,
-        // }],
+                pathGroupsExcludedImportTypes: [],
+                warnOnUnassignedImports: true,
+            }],
 
-        "tailwindcss/no-custom-classname": "off",
-        "unicorn/better-regex": "error",
-        "unicorn/consistent-destructuring": "error",
-        "unicorn/prefer-includes": "warn",
-    },
-}];
+            "jsx-a11y/media-has-caption": "off",
+            "no-unused-vars": "off",
+
+            "react-refresh/only-export-components": ["warn", {
+                allowConstantExport: true,
+            }],
+
+            "react/jsx-boolean-value": "error",
+            "react/jsx-newline": "error",
+            "react/jsx-no-useless-fragment": "error",
+
+            "react/jsx-pascal-case": ["error", {
+                allowAllCaps: false,
+            }],
+
+            "react/jsx-sort-props": ["warn", {
+                callbacksLast: true,
+                multiline: "last",
+                shorthandFirst: true,
+                reservedFirst: true,
+            }],
+
+            "react/jsx-wrap-multilines": ["warn", {
+                arrow: "parens-new-line",
+                assignment: "parens-new-line",
+                condition: "parens-new-line",
+                declaration: "parens-new-line",
+                logical: "parens-new-line",
+                prop: "parens-new-line",
+                return: "parens-new-line",
+            }],
+
+            "react/prefer-read-only-props": "warn",
+            "react/prop-types": "off",
+            "react/react-in-jsx-scope": "off",
+
+            "react/self-closing-comp": ["error", {
+                component: true,
+                html: true,
+            }],
+
+            "sort-imports": ["error", {
+                ignoreCase: true,
+                ignoreDeclarationSort: true,
+            }],
+
+            "perfectionist/sort-imports": "off",
+
+            "perfectionist/sort-interfaces": ["warn", {
+                type: "natural",
+                partitionByNewLine: true,
+            }],
+
+            "perfectionist/sort-jsx-props": "off",
+            "perfectionist/sort-named-imports": "off",
+
+            "perfectionist/sort-objects": ["warn", {
+                type: "natural",
+                ignorePattern: ["tableActions"],
+                partitionByNewLine: true,
+            }],
+
+            "tailwindcss/no-custom-classname": "off",
+            "unicorn/better-regex": "error",
+            "unicorn/consistent-destructuring": "error",
+            "unicorn/prefer-includes": "warn",
+        },
+    }
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import eslint from '@nabla/vite-plugin-eslint';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import { checker } from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    eslint(),
-    checker({ typescript: true }),
-  ],
-})
+    plugins: [
+        react(),
+        tsconfigPaths(),
+        eslint(),
+        checker({ typescript: true })
+    ]
+});
