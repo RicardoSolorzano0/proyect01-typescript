a04876ec2eb2944e2f6fdfef3de7814ffb2caf68
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 11:56:33 -0600

    arreglando linter usertypepage

--- src/ui/pages/UsersTypePage/UsersTypePage.tsx
+++ src/ui/pages/UsersTypePage/UsersTypePage.tsx
@@ -1,131 +1,150 @@
-import { Button, App, Switch } from "antd";
-import { TypeUser } from "@/types/TypeUsers";
-import { UserTypeForm } from "@/forms/UserTypeForm/UserTypeForm";
-import { useDeleteUserTypeMutation, useSelectPaginatedUserTypesQuery } from "@/api/services/userTypes";
-import { useState } from "react";
-import { OptionInGetQuerys } from "@/types/generalTypes";
-import { Input } from "antd"
-import { useDebounce } from "@/hooks/debounce";
-import { useTranslation } from "react-i18next";
-import { globalT } from "@/i18n";
-import { EntityTable } from "@/ui/components/EntityTable";
-import { columns } from "./table/columns";
-import { useHandlePaginatedData, usePagination } from "@/hooks/paginate";
+import { App, Button, Input, Switch } from 'antd';
+import { useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
+import { columns } from './table/columns';
+import { useDeleteUserTypeMutation, useSelectPaginatedUserTypesQuery } from '@/api/services/userTypes';
+import { UserTypeForm } from '@/forms/UserTypeForm/UserTypeForm';
+import { useDebounce } from '@/hooks/debounce';
+import { useHandlePaginatedData, usePagination } from '@/hooks/paginate';
+import { globalT } from '@/i18n';
+import type { OptionInGetQuerys } from '@/types/generalTypes';
+import type { TypeUser } from '@/types/TypeUsers';
+import { EntityTable } from '@/ui/components/EntityTable';
 
 const { useApp } = App;
 
 export const UsersTypePage = () => {
-  const { t } = useTranslation('usersTypes');
-  const [text, setText] = useState("");
-  const debouncedText = useDebounce(text);
-  const [option, setOption] = useState<OptionInGetQuerys>("active");
-  const page = usePagination();
-  // const [page, setPage] = useState(1);
-  const { data: dataPaginate, isLoading: isLoadingPaginate, isFetching: isFetchingPaginate } = useSelectPaginatedUserTypesQuery({ option, limit: 10, page, name: debouncedText });
+    const { t } = useTranslation('usersTypes');
+    const [text, setText] = useState('');
+    const debouncedText = useDebounce(text);
+    const [option, setOption] = useState<OptionInGetQuerys>('active');
+    const page = usePagination();
+    // const [page, setPage] = useState(1);
+    const { data: dataPaginate, isFetching: isFetchingPaginate, isLoading: isLoadingPaginate } = useSelectPaginatedUserTypesQuery({ limit: 10, name: debouncedText, option, page });
 
-  const { data, pagination } = useHandlePaginatedData(dataPaginate);
+    const { data, pagination } = useHandlePaginatedData(dataPaginate);
 
-  //const [deleteUserType, {data:dataDelete, error:errorDelete, isLoading:isLoadingDelete}] = useDeleteUserTypeMutation()
-  const [deleteUserType, { isLoading: isLoadingDelete }] = useDeleteUserTypeMutation()
-  const { modal, notification } = useApp();
-  // const [typeUsers, setTypeUsers] = useState<TypeUser[]>(data as TypeUser[]);
+    //const [deleteUserType, {data:dataDelete, error:errorDelete, isLoading:isLoadingDelete}] = useDeleteUserTypeMutation()
+    const [deleteUserType, { isLoading: isLoadingDelete }] = useDeleteUserTypeMutation();
+    const { modal, notification } = useApp();
+    // const [typeUsers, setTypeUsers] = useState<TypeUser[]>(data as TypeUser[]);
 
-  const handleCreate = () => {
-    const mdl = modal.info({
-      title: t("form.titles.create"),
-      content: (
-        <UserTypeForm
-          handleCancel={() => mdl.destroy()}
-        />
-      ),
-      cancelButtonProps: {
-        style: { display: "none" },
-      },
-      okButtonProps: {
-        style: { display: "none" },
-      },
-    });
-  };
+    const handleCreate = () => {
+        const mdl = modal.info({
+            cancelButtonProps: {
+                style: { display: 'none' }
+            },
+            content: (
+                <UserTypeForm
+                    handleCancel={ () => mdl.destroy() }
+                />
+            ),
+            okButtonProps: {
+                style: { display: 'none' }
+            },
+            title: t('form.titles.create')
+        });
+    };
 
-  const handleEdit = (record: TypeUser) => {
-    const mdl = modal.info({
-      // title: `Editar tipo de usuario ${record.name}`,
-      title: t("form.titles.edit", { typeUser: record.name }),
-      content: (
-        <UserTypeForm
-          typeUser={record}
-          handleCancel={() => mdl.destroy()}
-        />
-      ),
-      okButtonProps: {
-        style: { display: "none" },
-      },
-      cancelButtonProps: {
-        style: { display: "none" },
-      },
-    });
-  };
+    const handleEdit = (record: TypeUser) => {
+        const mdl = modal.info({
+            cancelButtonProps: {
+                style: { display: 'none' }
+            },
+            content: (
+                <UserTypeForm
+                    handleCancel={ () => mdl.destroy() }
+                    typeUser={ record }
+                />
+            ),
+            okButtonProps: {
+                style: { display: 'none' }
+            },
+            // title: `Editar tipo de usuario ${record.name}`,
+            title: t('form.titles.edit', { typeUser: record.name })
+        });
+    };
 
-  const handleDelete = (record: TypeUser) => {
-    modal.confirm({
-      title: t("form.titles.delete", { typeUser: record.name }),
-      content: t("form.contents.delete", { typeUser: record.name }),
-      onOk: async () => {
-        try {
-          await deleteUserType(record.id).unwrap();
-          notification.success({
-            message: t("messages.success.delete"),
-            description: t("messages.success.deleteDescription", { typeUser: record.name }),
-            duration: 2,
-          });
-        } catch (error) {
-          const parsedError = error as { error: string };
-          notification.error({
-            message: "Error",
-            description: t(`messages.errors.${parsedError.error}`),
-            duration: 2,
-          })
-        }
-      },
-    });
-  };
+    const handleDelete = (record: TypeUser) => {
+        modal.confirm({
+            content: t('form.contents.delete', { typeUser: record.name }),
+            onOk: async () => {
+                try {
+                    await deleteUserType(record.id).unwrap();
+                    notification.success({
+                        description: t('messages.success.deleteDescription', { typeUser: record.name }),
+                        duration: 2,
+                        message: t('messages.success.delete')
+                    });
+                } catch (error) {
+                    const parsedError = error as { error: string; };
+                    notification.error({
+                        description: t(`messages.errors.${parsedError.error}`),
+                        duration: 2,
+                        message: 'Error'
+                    });
+                }
+            },
+            title: t('form.titles.delete', { typeUser: record.name })
+        });
+    };
 
-  const handleSwitch = (record: boolean) => {
-    setOption(record ? "active" : "inactive");
-  }
+    const handleSwitch = (record: boolean) => {
+        setOption(record ? 'active' : 'inactive');
+    };
 
-  const loading = isLoadingPaginate || isFetchingPaginate || isLoadingDelete;
+    const loading = isLoadingPaginate || isFetchingPaginate || isLoadingDelete;
 
-  const tableActions = {
-    update: handleEdit,
-    delete: handleDelete
-  }
+    const tableActions = {
+        update: handleEdit,
+        delete: handleDelete
+    };
 
-  return (
-    <div>
-      <div className="flex justify-between items-center flex-wrap">
-        <Button type="primary" onClick={handleCreate}>
-          {t("page.addButton")}
-        </Button>
-        <div className="flex gap-4 items-center">
-          <Input placeholder={t("page.searchBy")} allowClear onChange={(e) => { setText(e.target.value) }} />
-          <Switch className="w-44" defaultChecked checkedChildren="Activos" unCheckedChildren="Eliminados" onChange={handleSwitch} />
-        </div>
-      </div>
-      <br />
+    return (
+        <div>
+            <div className='flex flex-wrap items-center justify-between'>
+                <Button
+                    type='primary'
+                    onClick={ handleCreate }
+                >
+                    {t('page.addButton')}
+                </Button>
 
-      {loading ?
-        <p>{globalT("loading")}</p>
-        :
-        <>
-          <EntityTable
-            actions={tableActions}
-            columns={columns(t)}
-            data={data ?? []}
-            loading={loading}
-            pagination={pagination}
-          />
-          {/* <Table
+                <div className='flex items-center gap-4'>
+                    <Input
+                        allowClear
+                        placeholder={ t('page.searchBy') }
+                        onChange={ e => {
+                            setText(e.target.value); 
+                        } }
+                    />
+
+                    <Switch
+                        defaultChecked
+                        checkedChildren='Activos'
+                        className='w-44'
+                        unCheckedChildren='Eliminados'
+                        onChange={ handleSwitch }
+                    />
+                </div>
+            </div>
+
+            <br />
+
+            {loading ?
+                <p>{globalT('loading')}</p>
+                : (
+                    <>
+                        <EntityTable
+                            actions={ tableActions }
+                            columns={ columns(t) }
+                            data={ data ?? [] }
+                            loading={ loading }
+                            pagination={ pagination }
+                        />
+
+                        {/* <Table
             rowKey={(record) => record.id}
             dataSource={dataPaginate?.data}
             pagination={false}
@@ -167,8 +186,8 @@ export const UsersTypePage = () => {
               }}
             />
           </div> */}
-        </>
-      }
-    </div>
-  );
+                    </>
+                )}
+        </div>
+    );
 };
