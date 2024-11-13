4b2248a48d747702cb06afee65425c54f7f3beb5
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-11-13 10:58:33 -0600

    arreglando linter en traduccion

--- src/i18n/locales/en/index.ts
+++ src/i18n/locales/en/index.ts
@@ -1,13 +1,13 @@
+import animals from './animals.json';
 import common from './common.json';
+import login from './login.json';
 import users from './users.json';
 import usersTypes from './usersTypes.json';
-import animals from './animals.json';
-import login from "./login.json";
 
 export const enLocales = {
-    common,
-    users,
-    usersTypes,
     animals,
-    login
-}
+    common,
+    login,
+    users,
+    usersTypes
+};
